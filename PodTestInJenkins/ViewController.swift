011b314d13d2e084c9dfe4378bc13e26a8c11a76
SMSC-PMacmini <smscmacmini2@macmini2.local>
2019-01-17 11:43:32 +0530

    add one podfiel

---
+++ PodTestInJenkins/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PodTestInJenkins
+//
+//  Created by SMSC-PMacmini on 17/01/19.
+//  Copyright © 2019 phanindhra B V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Alomafire")
+        
+        
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-PodTestInJenkins/Pods-PodTestInJenkins-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PodTestInJenkins : NSObject
+@end
+@implementation PodsDummy_Pods_PodTestInJenkins
+@end
